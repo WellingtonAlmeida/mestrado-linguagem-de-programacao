e628afa62c1c5aed248366d6cdf1ba2ddbe0aec8
Wellington <wgawebmail@gmail.com>
2023-04-03 15:21:44 -0300

    [chore:2065] aprimora solucao para atender requisitos de time limit

--- 2065-supermarket-line.py
+++ 2065-supermarket-line.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 number_of_cashiers, number_of_clients = input().split()
-times = input().split()
-items = input().split()
+times = [int(i) for i in input().split()]
+items = [int(i) for i in input().split()]
 total_time = [0]*len(times)
 current_cashier_index = 0
-for client_index in range(len(items)):
-    total_time[current_cashier_index] += int(items[client_index])*int(times[current_cashier_index])
-    current_cashier_index = current_cashier_index + 1 if current_cashier_index < len(times) - 1 else 0
-total_time.sort()
-print(total_time.pop())
+for client_items in items:
+    total_time[current_cashier_index] += client_items*times[current_cashier_index]
+    current_cashier_index = total_time.index(min(total_time))
+print(max(total_time))
