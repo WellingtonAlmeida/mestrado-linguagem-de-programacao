db6aa4b46298ae1bc53ec2ef64506f69e46fed13
Wellington <wgawebmail@gmail.com>
2023-04-03 08:29:41 -0300

    [chore:2065] testa solucao sem dicionarios

--- 2065-supermarket-line.py
+++ 2065-supermarket-line.py
@@ -1,27 +1,11 @@
 # -*- coding: utf-8 -*-
-def get_total_time(cashier):
-    return cashier["total_time"]
-
-
-def get_id(cashier):
-    return cashier["id"]
-
-
-number_of_cashiers, number_of_clients = list([int(i) for i in input().split()])
-times = list([int(i) for i in input().split()])
-items = list([int(i) for i in input().split()])
-clients = []
-cashiers = []
-for i in range(number_of_clients):
-    client = {"id": i, "items": items[i]}
-    clients.append(client)
-for j in range(number_of_cashiers):
-    cashier = {"id": j, "time": times[j], "total_time": 0}
-    cashiers.append(cashier)
-while clients:
-    client = clients[0]
-    cashier = cashiers[0]
-    cashier["total_time"] += client["items"]*cashier["time"]
-    cashiers = sorted(cashiers, key=lambda c: (get_total_time(c), get_id(c)))
-    clients.remove(client)
-print(cashiers.pop()["total_time"])
+number_of_cashiers, number_of_clients = input().split()
+times = input().split()
+items = input().split()
+total_time = [0]*len(times)
+current_cashier_index = 0
+for client_index in range(len(items)):
+    total_time[current_cashier_index] += int(items[client_index])*int(times[current_cashier_index])
+    current_cashier_index = current_cashier_index + 1 if current_cashier_index < len(times) - 1 else 0
+total_time.sort()
+print(total_time.pop())
